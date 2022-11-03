dce48e7def51d2591d7a2dbc7ce9cffb9e49fc2c
Waleed Lugod <lugodwaleed@gmail.com>
2022-11-03 22:37:29 +0800

    syntax

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./helpers/router";
-import Halo from "./components/Halo.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
-		<Halo />
 	</React.StrictMode>
 );
