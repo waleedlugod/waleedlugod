e2d39b30e764622a053ee96dcd4dea38c7569eea
Waleed Lugod <lugodwaleed@gmail.com>
2022-11-06 18:21:30 +0800

    added socials svgs

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
 import "./Contact.css";
+import svgGithub from "../../assets/github.svg";
+import svgTwitter from "../../assets/twitter.svg";
+import svgFacebook from "../../assets/facebook.svg";
+import svgInstagram from "../../assets/instagram.svg";
 
 export default function Contact() {
 	return (
-		<div className="contact">
+		<div id="contact">
 			<Link to="/">Home</Link>
-			<h1>Contact</h1>
-			<br className="line" />
-			<div className="links">
-				<a href="mailto:lugodwaleed@gmail.com">lugodwaleed@gmail</a>
-				<a href="https://github.com/waleedlugod">github</a>
-				<a href="https://twitter.com/waleedlugod">twitter</a>
-				<a href="https://www.facebook.com/wwwlugod">facebook</a>
-				<a href="https://www.instagram.com/waleed.lugod/">instagram</a>
-			</div>
+			<h1 className="title">Contact</h1>
+			<div className="line" />
+			<p>
+				Mail me: <a href="mailto:lugodwaleed@gmail.com">•lugodwaleed@gmail•</a>
+			</p>
+			<p>
+				Find me:{" "}
+				<a className="link" href="https://github.com/waleedlugod">
+					<img src={svgGithub} alt="" />
+				</a>
+				<a className="link" href="https://twitter.com/waleedlugod">
+					<img src={svgTwitter} alt="" />
+				</a>
+				<a className="link" href="https://www.facebook.com/wwwlugod">
+					<img src={svgFacebook} alt="" />
+				</a>
+				<a className="link" href="https://www.instagram.com/waleed.lugod/">
+					<img src={svgInstagram} alt="" />
+				</a>
+			</p>
+			<div className="links"></div>
 		</div>
 	);
 }
