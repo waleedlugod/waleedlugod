96b03f39b83c0efdd6c22067f2292eae92cd8d81
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-18 23:11:14 +0800

    changed home icon

--- src/components/article.js
+++ src/components/article.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import Script from "next/script";
 import styles from "waleedLugod/styles/article.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export default function Article({ title, children }) {
   return (
     <>
-      <Link href="/">Home</Link>
+      <Script src="https://kit.fontawesome.com/2b3ddf982d.js" crossOrigin="anonymous" />
+      <Link className={styles.homeLink} href="/">
+        <FontAwesomeIcon icon={faHouse} />
+      </Link>
       <article className={styles.content}>
         <h1 className={styles.title}>{title}</h1>
         {children}
