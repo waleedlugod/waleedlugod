1874cbb497bc78bd5e5398c4386df3b31a0b5ead
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-18 12:02:29 +0800

    basic content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import styles from "waleedLugod/styles/utils.module.css";
 
 export default function About() {
   return (
-    <main>
-      <Link href="/">Home</Link>
-      <h1>About</h1>
-      <div className="line"></div>
-      <p>stuff stuff and other stuff</p>
-    </main>
+    <>
+      <Link className={styles.homeLink} href="/">
+        Home
+      </Link>
+      <article className={styles.content}>
+        <h1>About</h1>
+        <div className={styles.line}></div>
+        <p>stuff stuff and other stuff</p>
+      </article>
+    </>
   );
 }
