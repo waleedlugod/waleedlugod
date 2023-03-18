3c0de243764ebc354ffacb78a9faae98ad8c4c63
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-18 20:34:50 +0800

    work article

---
+++ src/pages/work.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Article from "waleedLugod/components/article.js";
+
+export default function Work() {
+  return (
+    <Article title="Work">
+      <p>👀</p>
+    </Article>
+  );
+}
