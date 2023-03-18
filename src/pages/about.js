92e079796313b34d908165ca5bcc52458086e533
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-19 03:00:03 +0800

    changed title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
-import Link from "next/link";
+import Head from "next/head";
 import Article from "waleedLugod/components/article";
 
 export default function About() {
   return (
-    <Article title="About">
-      <p>stuff stuff and other stuff</p>
-    </Article>
+    <>
+      <Head>
+        <title>Waleed Lugod | About</title>
+      </Head>
+      <Article title="About">
+        <p>stuff stuff and other stuff</p>
+      </Article>
+    </>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Article from "waleedLugod/components/article";
 import styles from "waleedLugod/styles/Contact.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,25 +7,30 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
   return (
-    <Article title="Contact">
-      <main className={styles.content}>
-        <section className={styles.section}>
-          <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
-          <a href="mailto:lugodwaleed@gmail.com">lugodwaleed@gmail.com</a>
-        </section>
-        <section className={styles.section}>
-          <FontAwesomeIcon className={styles.icon} icon={faMagnifyingGlass} />
-          <a href="https://github.com/waleedlugod">github</a>
-          &nbsp;/&nbsp;
-          <a href="https://www.linkedin.com/in/waleedlugod/">linkedin</a>
-          &nbsp;/&nbsp;
-          <a href="https://twitter.com/waleedlugod">twitter</a>
-          &nbsp;/&nbsp;
-          <a href="https://www.facebook.com/wwwlugod">facebook</a>
-          &nbsp;/&nbsp;
-          <a href="https://www.instagram.com/waleed.lugod/">instagram</a>
-        </section>
-      </main>
-    </Article>
+    <>
+      <Head>
+        <title>Waleed Lugod | Contact</title>
+      </Head>
+      <Article title="Contact">
+        <main className={styles.content}>
+          <section className={styles.section}>
+            <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+            <a href="mailto:lugodwaleed@gmail.com">lugodwaleed@gmail.com</a>
+          </section>
+          <section className={styles.section}>
+            <FontAwesomeIcon className={styles.icon} icon={faMagnifyingGlass} />
+            <a href="https://github.com/waleedlugod">github</a>
+            &nbsp;/&nbsp;
+            <a href="https://www.linkedin.com/in/waleedlugod/">linkedin</a>
+            &nbsp;/&nbsp;
+            <a href="https://twitter.com/waleedlugod">twitter</a>
+            &nbsp;/&nbsp;
+            <a href="https://www.facebook.com/wwwlugod">facebook</a>
+            &nbsp;/&nbsp;
+            <a href="https://www.instagram.com/waleed.lugod/">instagram</a>
+          </section>
+        </main>
+      </Article>
+    </>
   );
 }
