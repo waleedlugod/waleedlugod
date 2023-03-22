5e4af0bc6a2372165b339ad83eec657d085bbeef
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-22 17:14:33 +0800

    home responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,40 +11,30 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <header className="p-24 font-thin">
-          <h1 className="text-4xl">waleed</h1>
-          <h1 className="text-9xl uppercase">lugod</h1>
+      <main className="flex h-screen flex-col justify-between">
+        <header className="ml-8 mt-12 font-thin sm:ml-24 sm:mt-24">
+          <h1 className="text-xl sm:text-4xl">waleed</h1>
+          <h1 className=" text-6xl uppercase sm:text-9xl">lugod</h1>
         </header>
-        <ul
-          className={`absolute right-32 bottom-24 text-right font-['Playfair_Display'] text-8xl 
-          font-extrabold`}
+        <nav
+          className={`mb-12 mr-8 mt-auto flex flex-col font-['Playfair_Display'] text-5xl font-extrabold sm:mr-24 sm:mb-24 sm:text-7xl`}
         >
-          <li>
-            <Link
-              className="link inline-block after:top-[58%] after:border-t-[0.1em]"
-              href="/about"
-            >
-              about
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="link inline-block after:top-[58%] after:border-t-[0.1em]"
-              href="/projects"
-            >
-              projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="link after:border-t-[0.1em]inline- inline-block after:top-[58%]"
-              href="/contact"
-            >
-              contact
-            </Link>
-          </li>
-        </ul>
+          <Link className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]" href="/about">
+            about
+          </Link>
+          <Link
+            className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]"
+            href="/projects"
+          >
+            projects
+          </Link>
+          <Link
+            className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]"
+            href="/contact"
+          >
+            contact
+          </Link>
+        </nav>
       </main>
       <Screen title="Home" />
     </>
