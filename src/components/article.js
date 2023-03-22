8ad7529f4d07f75570e3afd091bcf776742d8403
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-22 17:54:27 +0800

    article responsiveness

--- src/components/article.js
+++ src/components/article.js
@@ -6,11 +6,11 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 export default function Article({ title, children }) {
   return (
     <>
-      <Link className="absolute top-12 left-12 text-3xl" href="/">
+      <Link className="absolute top-4 left-4 sm:top-10 sm:left-10 sm:text-3xl" href="/">
         <FontAwesomeIcon icon={faHouse} />
       </Link>
-      <article className="p-32">
-        <h1 className="text-9xl font-bold after:mt-8 after:mb-12 after:block after:h-[2px] after:w-full after:bg-black after:content-['']">
+      <article className="w-full p-12 sm:p-20">
+        <h1 className="text-4xl font-bold after:mt-4 after:mb-6 after:block after:h-[2px] after:w-full after:bg-black after:content-[''] sm:text-8xl sm:after:mt-8 sm:after:mb-12">
           {title}
         </h1>
         {children}
