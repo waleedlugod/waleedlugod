36497d6bd2808726200b90aba7a8e0c91eb7a253
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-22 22:27:58 +0800

    hover hitbox fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,23 +16,21 @@ export default function Home() {
           <h1 className="text-xl sm:text-4xl">waleed</h1>
           <h1 className=" text-6xl uppercase sm:text-9xl">lugod</h1>
         </header>
-        <nav
-          className={`mb-12 mr-8 mt-auto flex flex-col font-['Playfair_Display'] text-5xl font-extrabold sm:mr-24 sm:mb-24 sm:text-7xl`}
-        >
+        <nav className="mb-12 mr-8 mt-auto flex flex-col gap-3 font-['Playfair_Display'] text-5xl font-extrabold sm:mr-24 sm:mb-24 sm:text-7xl">
           <Link className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]" href="/about">
-            about
+            <div className="pointer-events-none">about</div>
           </Link>
           <Link
             className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]"
             href="/projects"
           >
-            projects
+            <div className="pointer-events-none">projects</div>
           </Link>
           <Link
             className="link ml-auto w-min after:top-[58%] after:border-t-[0.1em]"
             href="/contact"
           >
-            contact
+            <div className="pointer-events-none">contact</div>
           </Link>
         </nav>
       </main>
