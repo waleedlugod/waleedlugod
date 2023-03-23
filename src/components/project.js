8e2b2dd989206bc6afd093cd651f66a5c1360945
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-23 21:29:52 +0800

    position adjustment

--- src/components/project.js
+++ src/components/project.js
@@ -3,16 +3,21 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProjectLink({ href, title, meta }) {
   return (
-    <a className="group flex flex-wrap items-baseline space-x-2" href={href}>
+    <a className="group flex flex-wrap" href={href}>
       <h2
-        className="link inline-block
+        className="link mr-2 inline-block
                 text-2xl font-thin after:top-[calc(50%-1px)]
                 after:border-t-[0.03em] group-hover:after:scale-x-100 sm:text-5xl"
       >
         {title}
       </h2>
-      <h3 className="mt-auto inline-block text-xs font-semibold sm:text-lg">{meta}</h3>
-      <FontAwesomeIcon className="" icon={faArrowUpRightFromSquare} />
+      <h3 className="mt-auto flex items-baseline text-xs font-semibold sm:text-lg">
+        {meta}
+        <FontAwesomeIcon
+          className="ml-1 inline-block text-[0.625rem] sm:text-sm"
+          icon={faArrowUpRightFromSquare}
+        />
+      </h3>
     </a>
   );
 }
