95ed028ba151a1330f8aca0d98bfcb6b8f7c43cf
Waleed Lugod <lugodwaleed@gmail.com>
2023-03-23 21:18:27 +0800

    external link

--- src/components/project.js
+++ src/components/project.js
@@ -1,14 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 export default function ProjectLink({ href, title, meta }) {
   return (
-    <a className="group flex flex-wrap" href={href}>
+    <a className="group flex flex-wrap items-baseline space-x-2" href={href}>
       <h2
-        className="link mr-4 inline-block
+        className="link inline-block
                 text-2xl font-thin after:top-[calc(50%-1px)]
                 after:border-t-[0.03em] group-hover:after:scale-x-100 sm:text-5xl"
       >
         {title}
       </h2>
       <h3 className="mt-auto inline-block text-xs font-semibold sm:text-lg">{meta}</h3>
+      <FontAwesomeIcon className="" icon={faArrowUpRightFromSquare} />
     </a>
   );
 }
