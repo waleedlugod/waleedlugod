546b7e502d8c9faf6b230843224063cf5c908295
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-10 00:27:37 +0800

    added new project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,22 +3,25 @@ import Article from "waleedLugod/components/article";
 import ProjectLink from "waleedLugod/components/project";
 
 export default function Projects() {
-  return (
-    <>
-      <Head>
-        <title>Waleed Lugod | Projects</title>
-      </Head>
-      <Article title="Projects">
-        <ul className="space-y-5">
-          <li>
-            <ProjectLink
-              href="https://interactive.theguidon.com/2023/02/from-the-hill-to-edsa"
-              title="From the Hill to EDSA"
-              meta="The GUIDON | 2023"
-            />
-          </li>
-        </ul>
-      </Article>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Waleed Lugod | Projects</title>
+            </Head>
+            <Article title="Projects">
+                <ul className="space-y-5">
+                    <ProjectLink
+                        href="https://interactive.theguidon.com/2023/02/from-the-hill-to-edsa"
+                        title="From the Hill to EDSA"
+                        meta="The GUIDON | 2023"
+                    />
+                    <ProjectLink
+                        href="https://size-accurate-solar-system.vercel.app/"
+                        title="Solar System"
+                        meta="Personal | 2023"
+                    />
+                </ul>
+            </Article>
+        </>
+    );
 }
