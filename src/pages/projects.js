628956ff8d1c5123fc03e30d81095eb07c451deb
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-16 00:04:48 +0800

    reorder projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,14 +11,14 @@ export default function Projects() {
             <Article title="Projects">
                 <ul className="space-y-5">
                     <ProjectLink
-                        href="https://interactive.theguidon.com/2023/02/from-the-hill-to-edsa"
-                        title="From the Hill to EDSA"
-                        meta="The GUIDON | 2023"
-                    />
-                    <ProjectLink
                         href="https://size-accurate-solar-system.vercel.app/"
                         title="Solar System"
                         meta="Personal | 2023"
+                    />
+                    <ProjectLink
+                        href="https://interactive.theguidon.com/2023/02/from-the-hill-to-edsa"
+                        title="From the Hill to EDSA"
+                        meta="The GUIDON | 2023"
                     />
                 </ul>
             </Article>
