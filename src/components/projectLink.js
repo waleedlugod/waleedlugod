4db4e30bfc8279c70eaa2baa3e158ad09326372a
Waleed Lugod <lugodwaleed@gmail.com>
2025-03-27 13:57:06 +0800

    adjust project link hover; update dependencies

--- src/components/projectLink.js
+++ src/components/projectLink.js
@@ -6,14 +6,17 @@ export default function ProjectLink({ href, title, meta }) {
     <a className="group flex w-max flex-wrap" href={href}>
       <h2
         className="link mr-2 inline-block
-                text-2xl font-thin after:top-[calc(50%-1px)]
-                after:border-t-[0.03em] group-hover:after:scale-x-100 sm:text-5xl"
+                text-2xl font-thin after:top-[calc(50%+0.125rem)]
+                after:border-t-[0.125rem] group-hover:after:scale-x-100 sm:text-5xl"
       >
         {title}
       </h2>
       <h3 className="mt-auto flex items-baseline text-xs font-semibold sm:text-lg">
         {meta}
-        <FontAwesomeIcon className="ml-1 inline-block text-[0.625rem] sm:text-sm" icon={faArrowUpRightFromSquare} />
+        <FontAwesomeIcon
+          className="ml-1 inline-block text-[0.625rem] sm:text-sm"
+          icon={faArrowUpRightFromSquare}
+        />
       </h3>
     </a>
   );
